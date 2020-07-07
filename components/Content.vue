f43d09a75d5a8277d1de8f0975e51ce4f9c9ddae
OlaBrzychcy <ola.brzychcy@gmail.com>
2020-07-07 09:42:21 +0200

    data from api

--- components/Content.vue
+++ components/Content.vue
@@ -1,53 +1,68 @@
 <template>
     <view class="container">
-        <touchable-opacity :on-press="getLocation" class="my-button">
-            <text class="text-field-title">Get Location</text>
-        </touchable-opacity>
-        <text class="text-field-title" v-if="has_data">Location Object:</text>
-        <text v-if="has_data">{{ location }}</text>
-        <text class="text-field-title" v-if="has_data">Latitude Only:</text>
-        <text v-if="has_data">{{ latitude }}</text>
-        <text class="text-field-title" v-if="has_data">Longitude Only:</text>
-        <text v-if="has_data">{{ longitude }}</text>
-
+        <text class="text-field-title">Location Object:</text>
+        <text>{{ location }}</text>
+        <text class="text-field-title">Latitude Only:</text>
+        <text>{{ latitude }}</text>
+        <text class="text-field-title">Longitude Only:</text>
+        <text>{{ longitude }}</text>
         <text class="text-error">{{ errorMessage }}</text>
     </view>
 </template>
 <script>
     import * as Location from "expo-location";
     import * as Permissions from "expo-permissions";
+    import axios from 'axios';
+
 
     export default {
         name: 'Content',
-        data: function() {
+        data: function () {
             return {
                 location: {},
                 latitude: "",
                 longitude: "",
                 errorMessage: "",
-                has_data:false,
+                API_key: '57f44175e5284473edbdc5b9138c8a36',
+                data: {},
+                data_bool: false
+
             };
         },
+        created() {
+            this.getLocationWeather();
+        },
+
         methods: {
-            getLocation: function() {
-                this.has_data = false;
+
+            getLocationWeather: function () {
+                this.data_bool = false;
                 Permissions.askAsync(Permissions.LOCATION)
                     .then(status => {
                         if (!status.granted) {
                             this.errorMessage = "Permission to access location was denied";
                         } else if (status.granted) {
                             Location.getCurrentPositionAsync({}).then(location => {
-                                this.has_data = true;
                                 this.location = location;
                                 this.latitude = location.coords.latitude
                                 this.longitude = location.coords.longitude
                                 this.errorMessage = "";
+                                this.getWeather(this.latitude, this.longitude)
                             });
                         }
                     })
                     .catch(err => {
                         console.log(err);
                     });
+            },
+            getWeather: function (latitude, longitude) {
+                var url = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&units=metric&lang=pl&exclude=minutely&appid=${this.API_key}`
+                return axios.get(url).then((response) => {
+                    this.data = response.data;
+                    this.data_bool = true;
+                }).catch(function (error) {
+                    throw error;
+                })
             }
         }
     };
@@ -61,10 +76,12 @@
         justify-content: center;
         flex: 1;
     }
+
     .text-color-primary {
         color: blue;
     }
-    .my-button{
+
+    .my-button {
         background-color: lightgray;
         border-width: 1px;
         padding: 3%;
